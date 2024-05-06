c10f3fe4ee7ab80e26719913cb75ef2ed6675e98
Aryan Regmi <aryan_regmi98@protonmail.com>
2024-05-06 14:43:34 -0600

    Refactored layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use std::{
-    alloc::{alloc, Layout},
-    mem,
+    alloc::{self, Layout},
     ops::{Index, IndexMut},
     ptr::{slice_from_raw_parts, slice_from_raw_parts_mut, NonNull},
 };
 
-struct Matrix {
+pub struct Matrix {
     data: NonNull<f32>,
     nrows: usize,
     ncols: usize,
@@ -13,7 +12,7 @@ struct Matrix {
 }
 
 impl Matrix {
-    /// Converts to the linear index from a matrix index.
+    /// Converts to the vector index from a matrix index.
     pub fn vec_idx(row: usize, col: usize, num_cols: usize) -> usize {
         num_cols * row + col
     }
@@ -28,11 +27,15 @@ impl Matrix {
         }
     }
 
-    /// Creates a new, empty matrix with capacity for `nrows x ncols` elements.
+    /// Creates a new, empty matrix with capacity for `nrows * ncols` elements.
     pub fn with_capacity(nrows: usize, ncols: usize) -> Self {
+        if nrows == 0 || ncols == 0 {
+            return Self::new();
+        }
+
         let data = unsafe {
-            let layout = Layout::from_size_align(nrows * ncols, 2).expect("Invalid memory layout"); // NOTE: Choose better align
-            NonNull::new(mem::transmute(alloc(layout))).expect("Allocation failed")
+            let layout = Layout::array::<f32>(nrows * ncols).expect("Invalid memory layout");
+            NonNull::new(std::mem::transmute(alloc::alloc(layout))).expect("Allocation failed")
         };
         Self {
             data,
@@ -46,7 +49,7 @@ impl Matrix {
     ///
     /// # Panics
     /// * Panics if the number of rows or columns is zero.
-    /// * Panics if the length of the slice is not equal to `cols * rows`.
+    /// * Panics if the length of the slice is not equal to `nrows * ncols`.
     pub fn from_slice(nrows: usize, ncols: usize, elems: &[f32]) -> Self {
         // Input validation
         {
@@ -67,18 +70,24 @@ impl Matrix {
         out.ncols = ncols;
         for i in 0..nrows {
             for j in 0..ncols {
-                out[i][j] = elems[Self::vec_idx(i, j, ncols)];
+                out[(i, j)] = elems[Self::vec_idx(i, j, ncols)];
             }
         }
+
         out
     }
 
-    /// Creates a new matrix of the specified size filled with the `default_value`.
+    /// Creates a new matrix of the specified size filled with the `val`.
     ///
     /// # Panics
     /// * Panics if the number of rows or columns is zero.
-    pub fn with_value(nrows: usize, ncols: usize, default_value: f32) -> Self {
-        Self::from_slice(nrows, ncols, &vec![default_value; nrows * ncols])
+    pub fn with_value(nrows: usize, ncols: usize, val: f32) -> Self {
+        // Input validation
+        if nrows == 0 || ncols == 0 {
+            panic!("Invalid size: The matrix must have more than zero rows and columns")
+        }
+
+        Self::from_slice(nrows, ncols, &vec![val; nrows * ncols])
     }
 
     /// Creates an identity matrix of the specified size.
@@ -86,98 +95,170 @@ impl Matrix {
     /// # Panics
     /// * Panics if the size is zero.
     pub fn identity(size: usize) -> Self {
-        let mut mat = Self::with_value(size, size, 0.0);
+        let mut out = Self::with_value(size, size, 0.0);
         for i in 0..size {
             for j in 0..size {
                 if i == j {
-                    mat[i][j] = 1.0;
+                    out[(i, j)] = 1.0;
                 }
             }
         }
-        mat
+        out
     }
 
     /// Creates a matrix of the specified size, filled with `0`.
     ///
     /// # Panics
     /// * Panics if the number of rows or columns is zero.
-    pub fn zeros(rows: usize, cols: usize) -> Self {
-        Self::with_value(rows, cols, 0.0)
+    pub fn zeros(nrows: usize, ncols: usize) -> Self {
+        Self::with_value(nrows, ncols, 0.0)
     }
 
     /// Creates a matrix of the specified size, filled with `1`.
     ///
     /// # Panics
     /// * Panics if the number of rows or columns is zero.
-    pub fn ones(rows: usize, cols: usize) -> Self {
-        Self::with_value(rows, cols, 1.0)
+    pub fn ones(nrows: usize, ncols: usize) -> Self {
+        Self::with_value(nrows, ncols, 1.0)
     }
 
-    /// Returns the number of rows in the matrix.
-    pub fn nrows(&self) -> usize {
-        self.nrows
-    }
-
-    /// Returns the number of columns in the matrix.
-    pub fn ncols(&self) -> usize {
-        self.ncols
-    }
-
-    /// Returns the size of the matrix `(nrows, ncols)`.
+    /// Returns the size of the matrix: `(nrows, ncols)`.
     pub fn size(&self) -> (usize, usize) {
         (self.nrows, self.ncols)
     }
 
-    /// Returns the number of elements allocated for the matrix.
-    pub fn capacity(&self) -> usize {
-        self.capacity
+    /// Returns the number of rows in the matrix.
+    pub fn num_rows(&self) -> usize {
+        self.nrows
     }
 
-    /// Sets the specified row to the given row.
-    pub fn set_row(&mut self, idx: usize, row: &[f32]) {
-        self[idx].copy_from_slice(row);
+    /// Returns the number of columns in the matrix.
+    pub fn num_cols(&self) -> usize {
+        self.ncols
     }
 
-    /// Sets the specified column to the given column.
-    pub fn set_col(&mut self, idx: usize, col: &[f32]) {
-        for i in 0..self.nrows {
-            self[i][idx] = col[i];
-        }
-    }
-}
-
-impl Index<usize> for Matrix {
-    type Output = [f32];
-
-    fn index(&self, index: usize) -> &Self::Output {
+    /// Returns an immutable reference to the specified row of the matrix.
+    pub fn row(&self, idx: usize) -> &[f32] {
         // Input validation
-        if index >= self.nrows {
+        if idx >= self.nrows {
             panic!(
-                "Invalid Index: The row index ({}) must be less than the number of rows ({})",
-                index, self.nrows
+                "Invalid row: The index must be less than {} (was {})",
+                self.nrows, idx
             )
         }
 
         unsafe {
-            slice_from_raw_parts(self.data.as_ptr().add(index * self.ncols), self.ncols)
+            slice_from_raw_parts(self.data.as_ptr().add(idx * self.ncols), self.ncols)
+                .as_ref()
+                .expect("Invalid row index")
+        }
+    }
+
+    /// Returns a mutable reference to the specified row of the matrix.
+    pub fn row_mut(&mut self, idx: usize) -> &mut [f32] {
+        // Input validation
+        if idx >= self.nrows {
+            panic!(
+                "Invalid row: The index must be less than {} (was {})",
+                self.nrows, idx
+            )
+        }
+
+        unsafe {
+            slice_from_raw_parts_mut(self.data.as_ptr().add(idx * self.ncols), self.ncols)
+                .as_mut()
+                .expect("Invalid row index")
+        }
+    }
+
+    /// Returns an immutable reference to the specified column of the matrix.
+    pub fn col(&self, idx: usize) -> Vec<&f32> {
+        // Input validation
+        if idx >= self.ncols {
+            panic!(
+                "Invalid column: The index must be less than {} (was {})",
+                self.ncols, idx
+            )
+        }
+
+        let mut out = vec![];
+        for i in 0..self.nrows {
+            out.push(&self[(i, idx)])
+        }
+        out
+    }
+
+    /// Returns a mutable reference to the specified column of the matrix.
+    pub fn col_mut<'a>(&'a mut self, idx: usize) -> Vec<&'a mut f32> {
+        // Input validation
+        if idx >= self.ncols {
+            panic!(
+                "Invalid column: The index must be less than {} (was {})",
+                self.ncols, idx
+            )
+        }
+
+        let mut out = vec![];
+        let nrows = self.nrows;
+        let ncols = self.ncols;
+        for i in 0..nrows {
+            let val = unsafe {
+                self.data
+                    .as_ptr()
+                    .add(Self::vec_idx(i, idx, ncols))
+                    .as_mut()
+                    .expect("Invalid index")
+            };
+            out.push(val)
+        }
+        out
+    }
+}
+
+impl Index<(usize, usize)> for Matrix {
+    type Output = f32;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        // Input validation
+        {
+            let row = index.0;
+            let col = index.1;
+            if row >= self.nrows || col >= self.ncols {
+                panic!(
+                    "Invalid index: The row and col must be less than {} and {} respectively",
+                    self.nrows, self.ncols
+                )
+            }
+        }
+
+        unsafe {
+            self.data
+                .as_ptr()
+                .add(Self::vec_idx(index.0, index.1, self.ncols))
                 .as_ref()
                 .expect("Invalid index")
         }
     }
 }
 
-impl IndexMut<usize> for Matrix {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+impl IndexMut<(usize, usize)> for Matrix {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
         // Input validation
-        if index >= self.nrows {
-            panic!(
-                "Invalid Index: The row index ({}) must be less than the number of rows ({})",
-                index, self.nrows
-            )
+        {
+            let row = index.0;
+            let col = index.1;
+            if row >= self.nrows || col >= self.ncols {
+                panic!(
+                    "Invalid index: The row and col must be less than {} and {} respectively",
+                    self.nrows, self.ncols
+                )
+            }
         }
 
         unsafe {
-            slice_from_raw_parts_mut(self.data.as_ptr().add(index * self.ncols), self.ncols)
+            self.data
+                .as_ptr()
+                .add(Self::vec_idx(index.0, index.1, self.ncols))
                 .as_mut()
                 .expect("Invalid index")
         }
@@ -190,7 +271,7 @@ impl std::fmt::Debug for Matrix {
         for i in 0..self.nrows {
             f.write_str("\n")?;
             for j in 0..self.ncols {
-                f.write_fmt(format_args!(" {}", self[i][j]))?;
+                f.write_fmt(format_args!(" {:?}", self[(i, j)]))?;
             }
         }
         f.write_str("\n]")
@@ -202,23 +283,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_create_matrices() {
-        let mat = Matrix::new();
-        assert_eq!(mat.size(), (0, 0));
-        assert_eq!(mat.capacity(), 0);
+    #[ignore]
+    fn can_init_matrix() {
+        let mat = Matrix::from_slice(2, 3, &[1., 2., 3., 4., 5., 6.]);
+        dbg!(&mat);
+        // dbg!(&mat.row(0));
+        // dbg!(&mat.row(1));
+        // mat.row_mut(0)[0] = 55.0;
+        // dbg!(&mat);
+        // dbg!(&mat.col(0));
+        // dbg!(&mat.col(1));
+        // dbg!(&mat.col(2));
+        // *mat.col_mut(0)[1] = 55.0;
+        // dbg!(&mat);
 
-        let mat = Matrix::with_capacity(2, 3);
-        assert_eq!(mat.size(), (0, 0));
-        assert_eq!(mat.capacity(), 6);
+        let mat = Matrix::zeros(1, 3);
+        dbg!(mat);
 
-        let elems = [1., 2., 3., 4., 5., 6.];
-        let mat = Matrix::from_slice(2, 3, &elems);
-        for i in 0..mat.nrows() {
-            for j in 0..mat.ncols() {
-                assert_eq!(mat[i][j], elems[Matrix::vec_idx(i, j, mat.ncols())])
-            }
-        }
-        assert_eq!(mat.size(), (2, 3));
-        assert_eq!(mat.capacity(), 6);
+        let mat = Matrix::ones(2, 1);
+        dbg!(mat);
+
+        let mat = Matrix::identity(4);
+        dbg!(mat);
     }
 }
